49d6d165762acd390dc0f90ed65c015b1bb41da6
mrinmoycu1 <mrinmoy_cu@yahoo.co.in>
2022-01-28 15:11:03 -0500

    Add files via upload

---
+++ problem-66.py
@@ -0,0 +1,29 @@
+'''
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
+'''
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits)-1
+        while i >= 0:
+            if (digits[i] == 9):
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+            i -= 1    
+        return [1] + digits
